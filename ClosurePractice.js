f86a4d67b078e6c862d0e8ddd45a948209aabea3
Ashraf Uddin <ashraf.uddin@selise.com.bd>
2018-02-06 20:48:54 +0600

    added new files

---
+++ ClosurePractice.js
@@ -0,0 +1,81 @@
+// These are not closure example
+
+function sayHello(name){
+	var text = 'Hello ' + name;
+	function say(){
+		console.log(text);
+	} 
+	say();
+}
+sayHello('Joe');
+
+
+function sayHello(name){
+	var text = 'Hello ' + name;
+	var say = function (){			// here function is initailized and this function is a anonymoius function
+		console.log(text);
+	} 
+	say();
+}
+sayHello('Joe');
+
+
+
+// notes about  JS closure
+
+//In JavaScript, if you use the function keyword inside another function, you are creating a closure.
+//In JavaScript, whenever you declare a function inside another function, the inside function(s) is/are recreated again each time the outside function is called.
+//The magic is that in JavaScript a function reference also has a secret reference to the closure it was created in — similar to how delegates are a method pointer plus a secret reference to an object.
+
+
+// Closure examples :
+
+// Intro
+function sayHello(name){
+
+	function say1(){
+	 	console.log("Hey " + name);
+	}
+	//say1();
+	return say1;
+	console.log("sayHello is done");
+}
+
+var say = sayHello("Joe");  // say2  gets a referense to the inner functions
+say();                      // now say2 will execute the say()
+say();
+
+
+//local variables are kept by reference
+
+function a(){
+	var num = 42;
+	num++;			//num is increamented
+	function b() {
+		// body...
+		console.log(num);
+	}
+	return b;
+	
+}
+
+var x = a();
+x();
+
+//One variation 
+
+function a(){
+	var num = 42;
+	
+	function b() {
+		// body...
+		console.log(num);
+	}
+	console.log("xxxxxxx");
+	return b;				//num is never increamented as function always returns
+	num++;			
+}
+
+var x = a();
+x();
+
