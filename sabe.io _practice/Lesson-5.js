65052966d6b87153aeea840c6360d9e327ccda8e
_neo_ <ashrafuddin160@gmail.com>
2018-02-11 08:48:29 +0600

    added

---
+++ sabe.io _practice/Lesson-5.js
@@ -0,0 +1,22 @@
+// Arithmatic operators
+
+//
+var x = 7;
+x += 8;
+console.log(x);
+
+// order of operations 
+var x = (5*3) + 10/2;
+console.log(x);
+
+// Math properties
+// we can access different mathathematical constants using math properites
+var pi = Math.PI;
+console.log(pi);
+
+// 
+var euler = Math.E;
+console.log(euler);
+
+// Math methods
+
