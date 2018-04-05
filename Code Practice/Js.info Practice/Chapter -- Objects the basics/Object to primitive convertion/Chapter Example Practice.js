77d020a453028dc641abeb317ade11ae7a959851
neo <ashrafuddin160@gmail.com>
2018-04-05 13:25:36 +0600

    added practice code

--- Code Practice/Js.info Practice/Chapter -- Objects the basics/Object to primitive convertion/Chapter Example Practice.js
+++ Code Practice/Js.info Practice/Chapter -- Objects the basics/Object to primitive convertion/Chapter Example Practice.js
@@ -7,7 +7,41 @@ let obj2 = {
 };
 let obj = obj1 + obj2;
 
-console.log(obj);
+console.log(+obj);
 
 let num = Number(obj1);
 console.log(num);
+
+let user= {
+    name : "John",
+    money : 1000,
+    [Symbol.toPrimitive](hint){
+        alert(`hint:"${hint}"`);
+        return hint == "string" ? `name : ${this.name}`: `money : ${this.name}`;
+    }
+};
+
+alert(user);
+alert(+user);
+alert(user + 500);
+
+let user = {
+    name: "John",
+    money: 1000,
+  
+    // for hint="string"
+    toString() {
+      return `{name: "${this.name}"}`;   // ${} is part string lateral to get tha value
+    },
+  
+    // for hint="number" or "default"
+    valueOf() {
+      return this.money;
+    }
+  
+  };
+  
+  alert(user); // toString -> {name: "John"}
+  alert(+user); // valueOf -> 1000
+  alert(user + 500); // valueOf -> 1500
+
